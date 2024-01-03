5d22979d3aabd5a076f88b32ec09a0baa1761162
Gabriel Carroll <gabrielcarroll11@gmail.com>
2024-01-03 12:55:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,36 +21,33 @@ while True:
         if team1 != team2:
             break
         
+    
     # Create a sub loop for asking the user what team they are going to bet on
     while True:
-        
         # Ask the user which team they would like to bet on
         team = int(input("What team would you like to bet on? " + team1 + ", or " + team2 + "? (1, 2) "))
-        
         # If team is equal to 1 break out of the loop
         if team == 1:
             break
-        
         # If team is equal to 2 break out of the loop
         elif team == 2:
             break
-        
         # If user responce is not one or two redo the loop
         else:
             ("Invalid Answer: Please retry. ")
     
+    
     # Create a sub loop while asking the user how much they would like to bet
     while True:
-        # Ask the user how mucjh they would like to bet
+        # Ask the user how much they would like to bet
         amount = int(input("How much would you like to bet? £"))
-        
         # If the user bets over £100 the loop will continue and the will be reprompted
         if amount > 100:
             print ("Invalid bet amount: Max bet of £100. ")
-            
         # If the amount they would like to bet is under £100 the loop will break
         elif amount <= 100:
             break
+    
     
     # Randomise the teams scores
     team1score =  random.randint(1, 10)
